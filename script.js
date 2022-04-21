8e2a66b0de24edbd2f680f193e58e0b6f920f143
Ethan Myos <myos0004@umn.edu>
2022-04-21 17:52:13 -0500

    calculator is now fully functional

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
-/*
-const plus = document.getElementById('plus');
-const minus = document.getElementById('minus');
-const times = document.getElementById('multiply');
-const divide = document.getElementById('divide');
-*/
+
 const equals = document.getElementById('equals');
-const decimal = document.getElementById('decimal');
 const clear = document.getElementById('clear');
 const del = document.getElementById('delete');
 const display = document.getElementById('display');
 const results = document.getElementById('results');
-const container = document.getElementById('container');
 // variable to keep track of display value before operator is chosen
 let num = '';
+// keep track of other operand
 let num2 = '';
+// keep track of which operator is being used
 let operator = '';
+// keep track of computation answer to pass into another operation
 let answer = '';
 // when an operator is clicked, 
 operators.forEach((op) => {
     op.addEventListener('click', () => {
-        // if there are two numbers stored, do math
-        /*
+        // if there are two numbers stored, do math with current operator, then switch operator
         if(num != '' && num2 != '') {
-            operator = op.value;
             doMath();
+            operator = op.value;
         }
-        */
         // save the number before the operator into a new variable
         if(num != '') num2 = num;
         // change the operator to the one that was clicked on
@@ -51,33 +45,40 @@ numbers.forEach((number) => {
 clear.addEventListener('click', () => {
     display.textContent = '0';
     num = '';
+    num2 = '';
     operator = '';
     results.textContent = '';
 })
 // delete the latest number clicked by the user and display it
 del.addEventListener('click', () => {
+    if(display.textContent == '0') return;
     display.textContent = display.textContent.slice(0,-1);
     if(num) num = num.slice(0,-1);
 })
-
+// do the current computation and display the results
 equals.addEventListener('click', () => {
-    answer = operate(operator, parseFloat(num), parseFloat(num2));
-    display.textContent = answer;
-    results.textContent = num + operator + num2 + "=";
-    num = answer;
+    doMath();
 })
-
-function add(x,y) {
-    return x + y;
+// function to take in two numbers and an operator and display the result of the math
+function doMath() {
+    answer = operate(operator, parseFloat(num2), parseFloat(num));
+    display.textContent = answer;
+    results.textContent = num2 + operator + num + "=";
+    num = answer;
+    num2 = '';
 }
+// functions to carry out simple arithmetic
+let add = (x,y) => x + y;
 let subtract = (x,y) => x - y;
 let multiply = (x,y) => x * y;
-let division = (x,y) => x / y;
-
+function division(x,y) {
+    if (y == 0) return 'err';
+    else return (x / y);
+}
+// function to decide which arithmetic operation is to be used based on chosen operator
 function operate(op, x, y) {
     if(op == "+") return (add(x,y)).toString();
     else if(op == "-") return (subtract(x,y)).toString();
     else if(op == "*") return (multiply(x,y)).toString();
-    else if(op == "/") return (divide(x,y)).toString();
-    else return 'wtf';
+    else return (division(x,y)).toString();
 }
